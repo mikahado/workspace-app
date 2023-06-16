822650d08a4e55143a7dc0006e515d485ead8ce7
Maika Harrell <mikahado@gmail.com>
2023-06-16 12:42:02 -0400

    adjusts fallback

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,13 @@ namespace :api do
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   
+end
+
+get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  !request.xhr? && request.format.html?
 end
 
 end
