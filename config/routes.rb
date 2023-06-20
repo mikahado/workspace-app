4e4dd0a0a3200d15be6726c1bfe112062d769d30
Maika Harrell <mikahado@gmail.com>
2023-06-20 13:13:06 -0400

    fallback test

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ end
 
 # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 
-# get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-#   !request.xhr? && request.format.html?
-# end
+  get '*path', to: "fallback#index", constraints: -> (request) do
+    !request.xhr? && request.format.html?
+  end
 
 end
